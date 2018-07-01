71d35f9492a2ffeb25f7e05d535679dffbe6066f
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-07-01 15:23:41 -0600

    reworked the app.js to feed from the new file structure.  most of the buttons don't want to work now, may need to get add'l work done for the css stuff in the components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,61 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Saved from "./pages/Saved";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import React, { Component } from 'react';
+import { Button, Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, Footer, NavLink, Container } from 'mdbreact';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './index.css';
+import SideBar from "./pages/SideBar";
+import Login from "./pages/Login.js";
+import CardsPage from "./pages/CardsPage";
+import ProductFeed from "./pages/ProductFeed";
+import TeamPage from './pages/TeamPage';
+import Landing from './pages/Landing.js'
+import Account from './pages/Account.js'
 
-const App = () => (
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/saved" component={Saved} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
-);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			collapsed: false,
+		};
+		this.handleTogglerClick = this.handleTogglerClick.bind(this);
+		this.handleNavbarClick = this.handleNavbarClick.bind(this);
+
+	}
+
+	handleTogglerClick() {
+		this.setState({
+			collapsed: !this.state.collapsed
+		});
+	}
+
+	handleNavbarClick() {
+		this.setState({
+			collapsed: false
+		});
+	}
+
+	render() {
+		const collapsed = this.state.collapsed;
+		const overlay = <div id="sidenav-overlay" style={{ backgroundColor: 'transparent' }} onClick={this.handleNavbarClick} />
+		return (
+<React.Fragment>
+
+				<SideBar />
+					
+					<Router>
+						<Switch>
+						<Route exact path="/" component={Landing}/>
+						<Route exact path="/team" component={TeamPage}/>
+						<Route exact path="/login" component={Login}/>
+						<Route exact path="/products" component={ProductFeed}/>
+						<Route exact path="/account" component={Account}/>
+
+						</Switch>
+					</Router>
+
+
+</React.Fragment>
+		);
+	}
+}
 
 export default App;
