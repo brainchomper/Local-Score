d0b9fae01514d8340e76df59e4f03627aceb1efb
Kevin Smith <kevin@kvnsmith.com>
2018-07-03 18:58:54 -0600

    account

--- client/src/pages/AccountTabs.js
+++ client/src/pages/AccountTabs.js
@@ -1,76 +1,81 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Container, Row, Col, TabPane, TabContent, Nav, NavItem, NavLink } from 'mdbreact';
+import { Container, Row, Col, TabPane, TabContent, Nav, NavItem, NavLink, Input, InputFile, InputSwitch } from 'mdbreact';
 import classnames from 'classnames';
+import Feed from './Feed';
+
 
 class TabsPage extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.toggleClassicTabs1 = this.toggleClassicTabs1.bind(this);
-    this.state = {
-      activeItemClassicTabs1: '1',
-    };
-  }
+		this.toggleClassicTabs1 = this.toggleClassicTabs1.bind(this);
+		this.state = {
+			activeItemClassicTabs1: '1',
+		};
+	}
 
-  toggleClassicTabs1(tab) {
-    if (this.state.activeItemClassicTabs1 !== tab) {
-      this.setState({
-        activeItemClassicTabs1: tab
-      });
-    }
-  }
+	toggleClassicTabs1(tab) {
+		if (this.state.activeItemClassicTabs1 !== tab) {
+			this.setState({
+				activeItemClassicTabs1: tab
+			});
+		}
+	}
 
 
-  render() {
-    return (
-      <Router>
-        <Container className="mt-4">
-          <Row>
-            <Col md="12">
-              <h2 className="mt-5">Classic tabs</h2>
-              <Nav classicTabs color="cyan">
-                <NavItem>
-                  <NavLink to="#" className={classnames({ active: this.state.activeItemClassicTabs1 === '1' })} onClick={() => { this.toggleClassicTabs1('1'); }}>
-                    Profile
+	render() {
+		return (
+			<Router>
+				<Container className="mt-4">
+					<Row>
+						<Col md="12">
+							<h2 className="mt-5 text-center">Account Profile Information</h2>
+							<Nav classicTabs color="cyan">
+								<NavItem>
+									<NavLink to="#" className={classnames({ active: this.state.activeItemClassicTabs1 === '1' })} onClick={() => { this.toggleClassicTabs1('1'); }}>
+										Profile
                   </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink to="#" className={classnames({ active: this.state.activeItemClassicTabs1 === '2' })} onClick={() => { this.toggleClassicTabs1('2'); }}>
-                  Follow
+								</NavItem>
+								<NavItem>
+									<NavLink to="#" className={classnames({ active: this.state.activeItemClassicTabs1 === '2' })} onClick={() => { this.toggleClassicTabs1('2'); }}>
+										Pending Transactions
                   </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink to="#" className={classnames({ active: this.state.activeItemClassicTabs1 === '3' })} onClick={() => { this.toggleClassicTabs1('3'); }}>
-                        Contact
+								</NavItem>
+								<NavItem>
+									<NavLink to="#" className={classnames({ active: this.state.activeItemClassicTabs1 === '3' })} onClick={() => { this.toggleClassicTabs1('3'); }}>
+										Completed Transactions
                   </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink to="#" className={classnames({ active: this.state.activeItemClassicTabs1 === '4' })} onClick={() => { this.toggleClassicTabs1('4'); }}>
-                    Be Awesome
-                  </NavLink>
-                </NavItem>
-              </Nav>
-              <TabContent className="card" activeItem={this.state.activeItemClassicTabs1}>
-                <TabPane tabId="1">
-                  <p>Quisquam aperiam, pariatur. Tempora, placeat ratione porro voluptate odit minima. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus reiciendis molestiae placeat unde eos molestias.</p>
-                </TabPane>
-                <TabPane tabId="2">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione porro voluptate odit minima.</p>
-                </TabPane>
-                <TabPane tabId="3">
-                  <p>Quisquam aperiam, pariatur. Tempora, placeat ratione porro voluptate odit minima. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus reiciendis molestiae placeat unde eos molestias.</p>
-                </TabPane>
-                <TabPane tabId="4">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione porro voluptate odit minima.</p>
-                </TabPane>
-              </TabContent>
-            </Col>
-          </Row>
-        </Container>
-      </Router>
-    );
-  }
+								</NavItem>
+							</Nav>
+							<TabContent className="card" activeItem={this.state.activeItemClassicTabs1}>
+								<TabPane tabId="1">
+									<Container>
+										<div className="row">
+											<div className="col-6"><Input label="First Name" /></div>
+											<div className="col-6"><Input label="Last Name" /></div></div>
+										<div className="row">
+											<div className="col"><Input label="What kind of coffee are you interested in?" /></div></div>
+									</Container>
+								</TabPane>
+								<TabPane tabId="2">
+									<Container>
+
+										<InputSwitch></InputSwitch>
+									</Container>
+								</TabPane>
+								<TabPane tabId="3">
+									<Container>
+<Feed />
+									</Container>
+								</TabPane>
+							</TabContent>
+						</Col>
+					</Row>
+				</Container>
+			</Router>
+		);
+	}
 }
 
 export default TabsPage;
