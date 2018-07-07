25d8ecc16253bd565f99a3854db182059ca4a65a
Kevin Smith <kevin@kvnsmith.com>
2018-07-07 12:47:52 -0600

    kvn pushing before leaving

--- client/src/pages/TransactionPage/TransactionPage.js
+++ client/src/pages/TransactionPage/TransactionPage.js
@@ -1,52 +1,68 @@
 import React from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Card, CardImage, Button, Fa, CardBody, CardTitle, CardText, Input } from 'mdbreact';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Card, CardImage, Button, Fa, CardBody, CardTitle, CardText, Input, InputNumeric } from 'mdbreact';
 class TransactionPage extends React.Component {
 	constructor(props) {
 		super(props);
 		this.toggle = this.toggle.bind(this);
 		this.state = {
-			dropdownOpen: false,
+			CustomerDropdown: false,
+			ProductDropdown: false
 		};
 	}
 	toggle() {
 		this.setState({
-			dropdownOpen: !this.state.dropdownOpen
+			CustomerDropdown: !this.state.CustomerDropdown,
+			ProductDropdown: !this.state.ProductDropdown
 		});
 	}
+
 	render() {
 		return (
-			<Card  cascade>
-				<CardBody cascade>
-					<CardTitle>Card title</CardTitle>
-					<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-						<DropdownToggle caret color="primary">
-							Customer
-          </DropdownToggle>
-						<DropdownMenu>
-							<DropdownItem href="#">.</DropdownItem>
-							<DropdownItem href="#">Another Action</DropdownItem>
-							<DropdownItem href="#">Something else here</DropdownItem>
-							<DropdownItem href="#">Something else here</DropdownItem>
-						</DropdownMenu>
-					</Dropdown>
-					<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-						<DropdownToggle caret color="primary">
-							Product
-          </DropdownToggle>
-						<DropdownMenu>
-							<DropdownItem href="#">.</DropdownItem>
-							<DropdownItem href="#">Another Action</DropdownItem>
-							<DropdownItem href="#">Something else here</DropdownItem>
-							<DropdownItem href="#">Something else here</DropdownItem>
-						</DropdownMenu>
-					</Dropdown>
-					<Input label="Example label" defaultValue="Price" />
-					<Button href="#">Button</Button>
-				</CardBody>
-				<div className="rounded-bottom mdb-color lighten-3 text-center pt-3">
+			<div className="row mx-auto"  style={{ maxWidth: '50%' }}>
+			<div className="col mt-4">
+			<Card>
+				<CardBody>
+					<CardTitle className="text-center">Create New Transaction</CardTitle>
 
-				</div>
+					<div className="row text-center">
+						<div className="col">
+							<Dropdown isOpen={this.state.CustomerDropdown} toggle={this.toggle} size="lg">
+								<DropdownToggle caret color="primary">
+									Customer
+          </DropdownToggle>
+								<DropdownMenu>
+									<DropdownItem href="#">.</DropdownItem>
+									<DropdownItem href="#">Another Action</DropdownItem>
+									<DropdownItem href="#">Something else here</DropdownItem>
+									<DropdownItem href="#">Something else here</DropdownItem>
+								</DropdownMenu>
+							</Dropdown>
+						</div>
+						<div className="col">
+							<Dropdown isOpen={this.state.ProductDropdown} toggle={this.toggle} size="lg">
+								<DropdownToggle caret color="primary">
+									Product
+          </DropdownToggle>
+								<DropdownMenu>
+									<DropdownItem href="#">.</DropdownItem>
+									<DropdownItem href="#">Another Action</DropdownItem>
+									<DropdownItem href="#">Something else here</DropdownItem>
+									<DropdownItem href="#">Something else here</DropdownItem>
+								</DropdownMenu>
+							</Dropdown>
+						</div>
+					</div>
+
+					<h3 className="text-center">Item Price $</h3>
+					<InputNumeric precision={2} value={10} step={0.01} className="mb-2" color="success"/>
+
+					<Button block color="success" size="lg" href="#">Submit New Transaction</Button>
+				</CardBody>
+
 			</Card>
+			</div>
+			</div>
+			
 
 		);
 	}
