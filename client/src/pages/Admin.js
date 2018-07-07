a000b9d85bf31cd4b6dfe995ab112cfcd2671710
Kevin Smith <kevin@kvnsmith.com>
2018-07-07 11:49:24 -0600

    kvn admin

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -4,24 +4,24 @@ const axios = require("axios");
 
 
 class FormsPage extends React.Component {
-	constructor(props){
-	super(props)
-	
-	this.state = {
-		FirstName: "",
-		LastName: "",
-		SocialKey: "",
-		Image: ""
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			FirstName: "",
+			LastName: "",
+			SocialKey: "",
+			Image: ""
+		}
+	};
+
+	handleInputChange(event) {
+		const target = event.target;
+		const { name, value } = target;
+		this.setState({
+			[name]: value
+		});
 	}
-};
-	
-handleInputChange(event) {
-	const target = event.target;
-const {name, value} = target;
-	this.setState({
-		[name]: value
-	});
-}
 	submit = event => {
 		event.preventDefault();
 		const user = {
@@ -30,26 +30,26 @@ const {name, value} = target;
 			SocialKey: this.state.SocialKey,
 			Image: this.state.Image
 		}
-		axios.put("/api/transactions/newUser", {user } ).then(results => console.log("results", results));
+		axios.put("/api/users/UserLogin", { user }).then(results => console.log("results", results));
 	}
 
 	handleInputChange = event => {
-    // Getting the value and name of the input which triggered the change
-    const { name, value } = event.target;
+		// Getting the value and name of the input which triggered the change
+		const { name, value } = event.target;
 
-    // Updating the input's state
-    this.setState({
-      [name]: value
-    });
-  };
+		// Updating the input's state
+		this.setState({
+			[name]: value
+		});
+	};
 
-  handleFormSubmit = event => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    event.preventDefault();
+	handleFormSubmit = event => {
+		// Preventing the default behavior of the form submit (which is to refresh the page)
+		event.preventDefault();
 
-    // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-    alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
-    this.setState({
+		// Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
+		alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
+		this.setState({
 			FirstName: "",
 			LastName: "",
 			SocialKey: "",
@@ -66,20 +66,20 @@ const {name, value} = target;
 						<form>
 							<p className="h4 text-center mb-4">User Creation</p>
 							<label htmlFor="FirstName" className="grey-text">First Name</label>
-							<input type="text" id="FirstName" className="form-control" 
-							value={this.state.firstName}
-            name="FirstName"
-            onChange={this.handleInputChange}
- />
+							<input type="text" id="FirstName" className="form-control"
+								value={this.state.firstName}
+								name="FirstName"
+								onChange={this.handleInputChange}
+							/>
 							<br />
 							<label htmlFor="LastName" className="grey-text">Last Name</label>
 							<input type="text" id="LastName" className="form-control" value={this.state.LastName} name="LastName" onChange={this.handleInputChange} />
 							<br />
 							<label htmlFor="SocialKey" className="grey-text">Social Key</label>
-							<input type="text" id="SocialKey" className="form-control" value={this.state.SocialKey} name="SocialKey" onChange={this.handleInputChange}/>
+							<input type="text" id="SocialKey" className="form-control" value={this.state.SocialKey} name="SocialKey" onChange={this.handleInputChange} />
 							<br />
 							<label htmlFor="Image" className="grey-text">Image</label>
-							<input type="text" id="Image" className="form-control" value={this.state.Image} name="Image" onChange={this.handleInputChange}/>
+							<input type="text" id="Image" className="form-control" value={this.state.Image} name="Image" onChange={this.handleInputChange} />
 							<div className="text-center mt-4">
 								<button className="btn btn-outline-warning" type="submit">Send<i className="fa fa-paper-plane-o ml-2" onClick={this.handleFormSubmit}></i></button>
 							</div>
