e665763f0d4be03966aa8a87345a2044542dcf51
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-07-11 21:15:54 -0600

    removed a bunch of the componenty things

--- client/src/pages/ProductFeed.js
+++ client/src/pages/ProductFeed.js
@@ -7,11 +7,9 @@ class ProductFeed extends Component {
 	constructor(props) {
 		super(props)
 		console.log(props)
-		const params = this.props.match.params.id
 
 		this.state = {
 			COMPLETED: [],
-			params: params,
 			userID: "",
 			queryResults: false
 		}
@@ -19,7 +17,7 @@ class ProductFeed extends Component {
 
 	componentDidMount() {
 		//call the api 
-		console.log("the params of the page rendering (AKA /transactionfeed/:id", this.props.match.params.id)
+		// console.log("the params of the page rendering (AKA /transactionfeed/:id", this.props.match.params.id)
 		console.log("Howdy");
 		axios
 			.get("/api/transactions/feed")
