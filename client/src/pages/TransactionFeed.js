009d040a87c9792e7cb6aac5b3887d5da3235a0e
Kevin Smith <kevin@kvnsmith.com>
2018-07-14 02:36:25 -0600

    kvn update

--- client/src/pages/TransactionFeed.js
+++ client/src/pages/TransactionFeed.js
@@ -9,19 +9,19 @@ export const TransactionFeed = props => {
 		<Container className="pb-3">
 		<Card testimonial className = "">
 			<div className="row">
-				<div className="col">
+				<div className="col d-inline-block">
 					<CardUp className="blue lighten-1" />
 					<Avatar className="mx-auto white">
 						<img src={Party2.Picture} className="rounded-circle z-depth-1 img-fluid" />
 					</Avatar>
 					<CardTitle className="text-center">{Party2.FirstName} {Party2.LastName}</CardTitle>
 				</div>
-				<div className="col">
+				<div className="col d-inline-block">
 					<h4 className="text-center pt-3">Bought <b>{ProductID.Name}</b> From</h4>
 					<CardImage className="img-fluid pb-3" src="./images/arrows.png" />
 
 					<Button block className="pt-3" color="success" href={ProductURL}>Learn More About This Product's History</Button></div>
-				<div className="col">
+				<div className="col d-inline-block">
 					<CardUp className="orange lighten-1" />
 					<Avatar className="mx-auto white">
 						<img src={Party1.Picture} className="rounded-circle z-depth-1 img-fluid" />
