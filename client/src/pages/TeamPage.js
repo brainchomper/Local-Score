fe4bec7936da598bfe3dc1073ed7e531335ee573
Kevin Smith <kevin@kvnsmith.com>
2018-07-19 20:54:24 -0600

    kvn update

--- client/src/pages/TeamPage.js
+++ client/src/pages/TeamPage.js
@@ -9,7 +9,7 @@ const TeamPage = () => {
 
 
 			{/* John*/}
-			<Col lg="3" md="6" className="mb-lg-0 mb-5">
+			<Col lg="4" md="6" className="mb-lg-0 mb-5">
 				<Avatar tag="img" src="./images/team/john.png" className="rounded-circle hoverable z-depth-1 img-fluid" alt="John" />
 				<h5 className="font-weight-bold mt-4 mb-3">John Stuart</h5>
 				<p className="text-uppercase blue-text">Full Stack Web Developer<br />API Brain</p>
@@ -28,11 +28,11 @@ const TeamPage = () => {
 			{/* /John */}
 
 			{/* Kevin */}
-			<Col lg="3" md="6" className="mb-lg-0 mb-5">
+			<Col lg="4" md="6" className="mb-lg-0 mb-5">
 				<Avatar tag="img" src="./images/team/kvn.png" className="hoverable rounded-circle z-depth-1 img-fluid" alt="Kevin" />
 				<h5 className="font-weight-bold mt-4 mb-3">Kevin Smith</h5>
 				<p className="text-uppercase blue-text">Full Stack Web Developer<br />Frontend Mastermind</p>
-				<p className="grey-text hoverable px-1">MERN Developer specializing in Front End Developement.<br /><br /><br />
+				<p className="grey-text hoverable px-1">MERN Developer specializing in Front End Developement.<br /><br />
 					<img src="./images/team/QRKevin.png" alt="Kevin Smith PNG" className="img-fluid px-4" />
 					<ul className="list-unstyled mb-0">
 						<a className="p-2 fa-lg" target="blank" href="https://github.com/brainchomper">
@@ -51,7 +51,7 @@ const TeamPage = () => {
 			{/* /Kevin */}
 
 				{/* Joe */}
-				<Col lg="3" md="6" className="mb-lg-0 mb-5">
+				<Col lg="4" md="6" className="mb-lg-0 mb-5">
 				<Avatar tag="img" src="./images/team/joe.png" className="rounded-circle z-depth-1 img-fluid hoverable" alt="Joe" />
 				<h5 className="font-weight-bold mt-4 mb-3">Joe Calderon</h5>
 				<p className="text-uppercase blue-text">Full Stack Web Developer<br />Backend Ninja</p>
@@ -70,12 +70,12 @@ const TeamPage = () => {
 			{/* /Joe */}
 
 			{/* Alex */}
-			<Col lg="3" md="6" className="mb-lg-0 mb-5">
-				<Avatar tag="img" src="./images/team/alex.png" className="hoverable rounded-circle z-depth-1 img-fluid" alt="Alex" />
-				<h5 className="font-weight-bold mt-4 mb-3">Alex Butler</h5>
-				<p className="text-uppercase blue-text">Full Stack Web Developer<br /> Something Here</p>
-				<p className="grey-text hoverable px-1">MERN Developer<br /><br /><br /><br />
-					<img src="./images/team/QRAlex.png" alt="Kevin Smith PNG" className="img-fluid px-4" />
+			{/* <Col lg="3" md="6" className="mb-lg-0 mb-5"> */}
+				{/* <Avatar tag="img" src="./images/team/alex.png" className="hoverable rounded-circle z-depth-1 img-fluid" alt="Alex" /> */}
+				{/* <h5 className="font-weight-bold mt-4 mb-3">Alex Butler</h5> */}
+				{/* <p className="text-uppercase blue-text">Full Stack Web Developer<br /> Something Here</p> */}
+				{/* <p className="grey-text hoverable px-1">MERN Developer<br /><br /><br /><br /> */}
+					{/* <img src="./images/team/QRAlex.png" alt="Kevin Smith PNG" className="img-fluid px-4" /> */}
 					{/* <ul className="list-unstyled mb-0"> */}
 						{/* <a className="p-2 fa-lg" target="blank" href="https://github.com/brainchomper"> */}
 							{/* <Fa icon="github" className="blue-text" /> */}
@@ -88,9 +88,9 @@ const TeamPage = () => {
 						{/* </a> */}
 					{/* </ul> */}
 					{/* <a href="https://kvnsmith.com">kvnsmith.com</a> */}
-					</p>
+					{/* </p> */}
 
-			</Col>
+			{/* </Col> */}
 			{/* /Alex */}
 				</Row>
 			
