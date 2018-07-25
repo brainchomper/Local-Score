8b3fc68799dadf70619aa2d48f93fc04aeaf1d04
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-07-25 16:34:57 -0700

    updated the transaction feed info to push through on the completed list.

--- client/src/pages/CompletedList.js
+++ client/src/pages/CompletedList.js
@@ -5,24 +5,29 @@ import BoardedFeed from "./BoardedFeed";
 
 
 const CompletedList = props => {
-
+	console.log("top level Completed Lsit");
 	if (props.props.queriesComplete === true && typeof props.props.COMPLETED !== "undefined") {
 
 		console.log(props, "props in the completed list")
 		return props.props.COMPLETED.map((each, i) => {
-			if (each.Party1._id !== each.Party2._id){
-				return 
+			console.log( "this is what each thing looks like in the console")
+			console.log("-------------------")
+			console.log(each.Party1._id === each.Party2._id)
+			console.log("-------------------")
 			
-				<TransactionFeed data={each} key = {i} />
+			if (each.Party1._id.toString() !== each.Party2._id.toString()) {
+				console.log("WTF MAN")
+				return (
+				<TransactionFeed data={each} key={i} />)
 			}
 
-			return <BoardedFeed data = {each} key = {i} />
+			else {	return <BoardedFeed data={each} key={i} /> }
 		})
 	} else {
 		return <div className="text-center">
-					<img src="https://local-score.herokuapp.com/images/loading.gif" alt="Loading" className="pt-6"/>
-		
-					</div>
+			<img src="https://local-score.herokuapp.com/images/loading.gif" alt="Loading" className="pt-6" />
+
+		</div>
 	}
 }
 export default CompletedList;
